b64f120efa853817df36436ed740553def0cb433
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-08-28 22:18:39 +1200

    add sign-in page and component

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './form-input.styles.scss'
+
+const FormInput = ({ handleChange, name, label, ...otherProps }) => (
+  <div className="group">
+    <input className="form-input" name={name} onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`} for={name}>
+        {label}
+      </label>
+    ) : null}
+  </div>
+)
+
+export default FormInput
