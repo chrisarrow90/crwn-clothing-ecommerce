a84b3d359a9d25df9367d33d0ed405f834294fe9
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-08-28 23:32:03 +1200

    integrate firebase into app. create signin page with sign in component. add sign in with google

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -6,7 +6,10 @@ const FormInput = ({ handleChange, name, label, ...otherProps }) => (
   <div className="group">
     <input className="form-input" name={name} onChange={handleChange} {...otherProps} />
     {label ? (
-      <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`} for={name}>
+      <label
+        className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}
+        htmlFor={name}
+      >
         {label}
       </label>
     ) : null}
