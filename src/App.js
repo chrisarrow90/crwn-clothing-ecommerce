41140581cd2c87fedc884cdcc527382ec8fc350b
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-08-28 22:19:08 +1200

    ammend import typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-import SignInAndSignUpPage from './pages/signin-in-and-sign-up/sign-in-and-sign-up.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 
 function App() {
