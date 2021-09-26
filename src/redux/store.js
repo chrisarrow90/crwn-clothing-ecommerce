7efbef97e3028b5f92a8e684dd28de3153d3c5a2
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-09-26 15:47:25 +1300

    move directory data into directory reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
+
 import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
@@ -9,4 +11,7 @@ const middlewares = [logger]
 // create store
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-export default store
+// persisted version of our store
+const persistor = persistStore(store)
+
+export { store, persistor }
