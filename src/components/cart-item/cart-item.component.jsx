714c92d4fd79a0b32b089c3ab8ae58e6af29a6ae
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-10-07 21:38:05 +1300

    convert checkout-item component to use styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-import './cart-item.styles.scss'
-
 import { CartItemContainer, ItemDetailsContainer, CartItemImage } from './cart-item.styles'
+
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <CartItemContainer>
     <CartItemImage src={imageUrl} alt="item" />
