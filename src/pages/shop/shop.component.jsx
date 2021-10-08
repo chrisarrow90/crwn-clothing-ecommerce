d44981c8d4cfc398b2213255f59b7477a66b895d
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-10-08 16:48:08 +1300

    refactor collection page to use container pattern

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
 
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
-import CollectionPage from '../collection/collection.component'
+import CollectionPageContainer from '../collection/collection.container'
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
-import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors'
 
-import WithSpinner from '../../components/with-spinner/with-spinner.component'
-
-const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 class ShopPage extends React.Component {
   componentDidMount() {
     const { fetchCollectionsStartAsync } = this.props
@@ -19,28 +14,19 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionsLoaded } = this.props
+    const { match } = this.props
 
     return (
       <div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={(props) => (
-            <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />
-          )}
-        />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
       </div>
     )
   }
 }
 
-const mapStateToProps = createStructuredSelector({
-  isCollectionsLoaded: selectIsCollectionsLoaded
-})
-
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage)
+export default connect(null, mapDispatchToProps)(ShopPage)
