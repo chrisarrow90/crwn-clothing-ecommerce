49322572086903b60f7239edb5f4c43044dd74bc
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-11-09 20:41:07 +1300

    update shop saga to use base saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,10 +1,10 @@
 import { all, call } from 'redux-saga/effects'
 
-import { fetchCollectionsStart } from './shop/shop.sagas'
+import { shopSagas } from './shop/shop.sagas'
 import { userSagas } from './user/user.sagas'
 import { cartSagas } from './cart/cart.sagas'
 
 export default function* rootSaga() {
   // all allows all sagas to run concurrently
-  yield all([call(fetchCollectionsStart), call(userSagas), call(cartSagas)])
+  yield all([call(userSagas), call(cartSagas), call(shopSagas)])
 }
