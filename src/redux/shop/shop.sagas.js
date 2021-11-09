2186352c29e73d378a1945828f39bd401e8b8f47
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-11-09 16:45:59 +1300

    change saga to use takeLatest

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,21 @@
+import { takeLatest, call, put } from 'redux-saga/effects'
+import { collection, getDocs } from 'firebase/firestore'
+import { db, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
+import ShopActionTypes from './shop.types'
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = collection(db, 'collections')
+    const snapshot = yield getDocs(collectionRef)
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+    yield put(fetchCollectionsSuccess(collectionsMap))
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message))
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync)
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
-import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+
+import { fetchCollectionsStart } from './shop/shop.sagas'
 
 import rootReducer from './root-reducer'
 
 // setup middlewares
-const middlewares = [thunk]
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [sagaMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
@@ -14,6 +17,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // create store
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+// saga
+sagaMiddleware.run(fetchCollectionsStart)
 
 // persisted version of our store
 const persistor = persistStore(store)
