da7e517d5b3535008b4040fd7637fd9e92f67dd9
Chris Arrowsmith <chrisarrowsmith@gmail.com>
2021-11-09 16:52:00 +1300

    add comment

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -3,5 +3,6 @@ import { all, call } from 'redux-saga/effects'
 import { fetchCollectionsStart } from './shop/shop.sagas'
 
 export default function* rootSaga() {
+  // all allows all sagas to run concurrently
   yield all([call(fetchCollectionsStart)])
 }
